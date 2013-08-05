7066cbe8e7f8477af6cab17b47af66300129c631
Jan Winkler <winkler@cs.uni-bremen.de>
2013-08-05 15:26:40 +0200

    Added script that packages experiment results nicely

---
+++ package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ~/.bashrc
+PLANLOGGER_DIR=`rospack find planlogger`
+cd ${PLANLOGGER_DIR}
+rm -Rf packaging
+mkdir -p packaging
+
+mongoexport -d db_exp_log -c col_exp_log -o packaging/planlog.json
+mongoexport -d roslog -c tf -o packaging/tf.json
+mongoexport -d roslog -c uima_trigger -o packaging/uima_trigger.json
+mongoexport -d roslog -c uima_uima_results -o packaging/uima_uima_results.json
+
+cp -r experiments/current-experiment/* packaging
+
+DATE_STRING=`date +"%Y-%m-%d-%H-%M-%S"`
+tar -czf log-packaged-${DATE_STRING}.tar.gz packaging/*
