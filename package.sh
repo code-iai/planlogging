2c148a0de21b2def2d51c82c8ed2508b1f8a8ba0
Jan Winkler <winkler@cs.uni-bremen.de>
2013-08-15 16:57:12 +0200

    Also export logged designators

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ cd ${PLANLOGGER_DIR}
 rm -Rf packaging
 mkdir -p packaging
 
-mongoexport -d db_exp_log -c col_exp_log -o packaging/planlog.json
+mongoexport -d roslog -c logged_designators -o packaging/logged_designators.json
 mongoexport -d roslog -c tf -o packaging/tf.json
 mongoexport -d roslog -c uima_trigger -o packaging/uima_trigger.json
 mongoexport -d roslog -c uima_uima_results -o packaging/uima_uima_results.json
